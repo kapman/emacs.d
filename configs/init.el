adfd466f31b3be927f4f98019acff644ad6504bc
Aamir Kapasi <aamir@helpshift.com>
2016-05-01 20:41:05 +0530

    Use paredit bindings for smart parens

--- configs/init.el
+++ configs/init.el
@@ -1,3 +1,4 @@
 (load-file (concat config-dir "smartparens-utils.el"))
+(load-file (concat config-dir "smartparens.el"))
 (load-file (concat config-dir "web-mode.el"))
 (load-file (concat config-dir "lisp-modes.el"))
